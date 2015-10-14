98d302cf85511d4b2e09824cd0c3e9765dd562a2
liuyuning <liuyuning2002@163.com>
2015-10-14 16:51:42 +0800

    Xcode7 build command line
    Xcode7 build command line

---
+++ config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+C=$(dirname $0)
+
+APP_PATH=/Applications/Xcode.app
+
+#echo 'Current path:"'${C}\"
+echo 'Xcode path:"'${APP_PATH}\"
+
+echo '== For command line tool(com.apple.product-type.tool, com.apple.package-type.mach-o-executable) =='
+mv -n ${APP_PATH}/Contents/PlugIns/IDEiOSSupportCore.ideplugin/Contents/Resources/Embedded-Shared.xcspec ${APP_PATH}/Contents/PlugIns/IDEiOSSupportCore.ideplugin/Contents/Resources/Embedded-Shared.orig.xcspec
+cp ${C}/Embedded-Shared.xcspec ${APP_PATH}/Contents/PlugIns/IDEiOSSupportCore.ideplugin/Contents/Resources/Embedded-Shared.xcspec
+echo '== Done =='
+
+echo '== For disable code signing on iOS device =='
+mv -n ${APP_PATH}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist ${APP_PATH}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.orig.plist
+cp ${C}/SDKSettings.plist ${APP_PATH}/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/SDKSettings.plist
+echo '== Done =='
